5bafbe1dbedbef433ae973a095bda4bd94654ca6
Eduardo Achar <eduardoachar1717@gmail.com>
2024-10-07 18:04:53 -0300

    a

--- README.md
+++
@@ -1,4 +0,0 @@
-# sistemasoperacionais1
-INE5412 - Sistemas Operacionais I
-
-https://www.youtube.com/watch?v=Vi78k-JOu5s
--- trabalho1/main.cpp
+++ trabalho1/main.cpp
@@ -1,25 +1,43 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+
+using namespace std;
 
 int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
-    int numJobs = profit.size(); // Number of jobs
-    vector<tuple<int, int, int>> jobs(numJobs);
     
-    for (int i = 0; i < numJobs; ++i) {
-        jobs[i] = {endTime[i], startTime[i], profit[i]};
+    // Vetor de processos
+    vector<tuple<int, int, int>> processos;
+
+    // Adiciona os processos no vetor
+    for (int i = 0; i < startTime.size(); i++) {
+        processos.push_back({endTime[i], startTime[i], profit[i]});
     }
-    
-    sort(jobs.begin(), jobs.end());
-    vector<int> dp(numJobs + 1);
-    
-    for (int i = 0; i < numJobs; ++i) {
-        auto [endTime, startTime, profit] = jobs[i];
-        
-        int latestNonConflictJobIndex = upper_bound(jobs.begin(), jobs.begin() + i, startTime, [&](int time, const auto& job) -> bool {
-            return time < get<0>(job);
-        }) - jobs.begin();
-        
-        dp[i + 1] = max(dp[i], dp[latestNonConflictJobIndex] + profit);
+
+    // Ordena os processos pelo tempo de término
+    sort(processos.begin(), processos.end());
+
+    // Vetor de lucro
+    vector<int> lucro(startTime.size());
+
+    // Calcula o lucro máximo
+    lucro[0] = get<2>(processos[0]);
+    for (int i = 1; i < processos.size(); i++) {
+        int profit = get<2>(processos[i]);
+        int l = 0, r = i - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (get<0>(processos[m]) <= get<1>(processos[i])) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        if (r >= 0) {
+            profit += lucro[r];
+        }
+        lucro[i] = max(profit, lucro[i - 1]);
     }
-    
-    return dp[numJobs];
+    return lucro.back();
 }
