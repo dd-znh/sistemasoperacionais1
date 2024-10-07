99a5736f6ffe66b526ca287ef9578fdb0f6860b7
Eduardo Achar <eduardoachar1717@gmail.com>
2024-10-07 20:10:56 -0300

    código feito

--- trabalho1/main.cpp
+++ trabalho1/main.cpp
@@ -10,15 +10,13 @@ int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& pro
     // Vetor de tarefas (processos)
     vector<tuple<int, int, int>> tarefas;
 
-    // Adiciona as tarefas no vetor (tempo de início, tempo de fim, lucro)
+    // Adiciona as tarefas no vetor (tempo de fim, tempo de início, lucro)
     for (int i = 0; i < startTime.size(); i++) {
-        tarefas.push_back({startTime[i], endTime[i], profit[i]});
+        tarefas.push_back({endTime[i], startTime[i], profit[i]});
     }
 
-    // Ordena as tarefas pelo tempo de início
-    sort(tarefas.begin(), tarefas.end(), [](const tuple<int, int, int>& a, const tuple<int, int, int>& b) {
-        return get<0>(a) < get<0>(b); // Ordena com base no primeiro elemento (tempo de início)
-    });
+    // Ordena as tarefas pelo tempo de término
+    sort(tarefas.begin(), tarefas.end());
 
     // Vetor que guarda o lucro máximo até cada tarefa
     vector<int> lucroMaximo(startTime.size());
@@ -28,27 +26,25 @@ int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& pro
 
     // Calcula o lucro máximo para cada tarefa
     for (int i = 1; i < tarefas.size(); i++) {
-
         // Lucro da tarefa atual
         int lucroAtual = get<2>(tarefas[i]);
 
-        // Inicializa as extremidades para busca binária
+        // Busca a última tarefa que não sobrepõe com a atual
         int l = 0, r = i - 1;
 
-        // Busca binária para encontrar a última tarefa que não se sobrepõe com a tarefa atual
+        // Busca binária para encontrar a última tarefa que termina antes da atual
         while (l <= r) {
             int meio = l + (r - l) / 2;
-            // Verifica se a tarefa no meio termina antes da tarefa atual começa
-            if (get<1>(tarefas[meio]) <= get<0>(tarefas[i])) {
-                l = meio + 1; // Move para a direita
+            if (get<0>(tarefas[meio]) <= get<1>(tarefas[i])) {
+                l = meio + 1;
             } else {
-                r = meio - 1; // Move para a esquerda
+                r = meio - 1;
             }
         }
 
         // Se encontrou uma tarefa compatível, soma o lucro dessa tarefa ao lucro atual
         if (r >= 0) {
-            lucroAtual += lucroMaximo[r]; // Adiciona o lucro da última tarefa compatível
+            lucroAtual += lucroMaximo[r];
         }
 
         // Atualiza o lucro máximo até a tarefa atual
